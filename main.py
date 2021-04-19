83f7620b6841caf0c4cd766467b3d0fd89085089
DoktorFish0078 <DoktorFish0078@mail.ru>
2021-04-20 02:03:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ def auto_recording_forms(URL, count_loops):
     options = webdriver.ChromeOptions()
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
 
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
 
